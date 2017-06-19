63feb34b98fce737b09eff1ce6119eae3e2f43be
Fernando Jorge Mota <f.j.mota13@gmail.com>
2017-06-18 22:26:03 -0300

    Add the Union operation, add method to fix menus as tabs are changed and a few other things

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "finite_automata_table.h"
+#include "finite_automata_tab.h"
 #include "regular_expression.h"
+#include <QInputDialog>
+#include "finite_automata.h"
+#include "operation_tab.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -12,13 +15,221 @@ MainWindow::MainWindow(QWidget *parent) :
     RegularExpression re("(ab|ac)*a?|(ba?c)*");
 
     FiniteAutomata f = re.getAutomata();
-    QGridLayout* layout = this->findChild<QGridLayout*>("gridLayout");
-    FiniteAutomataTable *w = new FiniteAutomataTable();
-    w->fromAutomata(f);
+    tabWidget = this->findChild<QTabWidget*>("tabWidget");
+//    FiniteAutomataTable *w = new FiniteAutomataTable();
+//    w->fromAutomata(f);
 //    cout << f.toASCIITable() << endl;
 //    cout << w->toAutomata().toASCIITable() << endl;
 //    cout << w->toAutomata().isEquivalent(f) << endl;
-    layout->addWidget(w);
+    FiniteAutomataTab *tab = new FiniteAutomataTab();
+    tab->fromAutomata(f);
+    tab->setObjectName("tab_Test");
+    tabWidget->addTab(tab, "Test");
+    fixMenus(0);
+}
+
+void MainWindow::createNewAutomata() {
+    FiniteAutomata f;
+    createNewAutomata(f);
+}
+
+QStringList MainWindow::getAutomatasNames() {
+    QStringList result;
+    int tabCount = tabWidget->count();
+    for (int tab=0; tab<tabCount; tab++) {
+        QWidget *widget = tabWidget->widget(tab);
+        if (dynamic_cast<FiniteAutomataTab*>(widget)) {
+            result.append(tabWidget->tabText(tab));
+        }
+    }
+    return result;
+}
+
+void MainWindow::doUnion() {
+    int currentIndex = tabWidget->currentIndex();
+    if (currentIndex < 0) {
+        return;
+    }
+    QWidget *tab = tabWidget->widget(currentIndex);
+    FiniteAutomataTab *fTab = dynamic_cast<FiniteAutomataTab*>(tab);
+    if (!fTab) {
+        return;
+    }
+    bool ok;
+    QString name = tabWidget->tabText(currentIndex);
+    QString f2Name = QInputDialog::getItem(
+                this,
+                "Select the automata",
+                "Select the automata to realize the union with '"+name+"':",
+                getAutomatasNames(),
+                0,
+                false,
+                &ok);
+    if (!ok) {
+        return;
+    }
+    QString opName = "op_"+name+"_union_"+f2Name;
+    OperationTab *op = tabWidget->findChild<OperationTab*>(opName);
+    if (op) {
+        tabWidget->setCurrentWidget(op);
+        return;
+    }
+    FiniteAutomata f = fTab->toAutomata();
+    cout << "tab_"+f2Name.toStdString() << endl;
+    FiniteAutomataTab *f2Tab = tabWidget->findChild<FiniteAutomataTab*>("tab_"+f2Name);
+    if (!f2Tab) {
+        return;
+    }
+    FiniteAutomata f2 = f2Tab->toAutomata();
+    QScrollArea *scroll = new QScrollArea(this);
+    op = new OperationTab(this);
+    scroll->setWidget(op);
+    scroll->setWidgetResizable(true);
+    op->setObjectName(opName);
+    op->addStep(this, f, "This is the automata '"+name+"':");
+    op->addStep(this, f2, "This is the automata '"+f2Name+"':");
+    op->addStep(this, f.doUnion(f2), "This is the result of the union between '"+name+"' and '"+f2Name+"':");
+    int opIndex = tabWidget->addTab(scroll, "Union between '"+name+"' and '"+f2Name+"'");
+    tabWidget->setCurrentIndex(opIndex);
+}
+
+void MainWindow::doIntersection() {
+
+}
+
+void MainWindow::fixMenus(int currentIndex) {
+    if (currentIndex < 0) {
+        return;
+    }
+    QWidget *tab = tabWidget->widget(currentIndex);
+    QList<QAction*> operations = menuBar()->findChild<QMenu*>("menuOperations")->actions();
+    QList<QAction*> tabs = menuBar()->findChild<QMenu*>("menuTabs")->actions();
+    if (dynamic_cast<FiniteAutomataTab*>(tab)) {
+        for (QAction* op: operations) {
+            op->setEnabled(op->objectName() != "actionConvert_to_Automata");
+        }
+        for (QAction* op: tabs) {
+            op->setEnabled(true);
+        }
+    } else {
+        for (QAction* op: operations) {
+            op->setEnabled(false);
+        }
+        for (QAction* op: tabs) {
+            op->setEnabled(false);
+        }
+    }
+}
+
+void MainWindow::setTabName() {
+    int currentIndex = tabWidget->currentIndex();
+    if (currentIndex < 0) {
+        return;
+    }
+    QWidget *tab = tabWidget->widget(currentIndex);
+    if (!(dynamic_cast<FiniteAutomataTab* >(tab) || dynamic_cast<OperationTab*>(tab))) {
+        return;
+    }
+    QString actualName = tabWidget->tabText(currentIndex);
+    QString name;
+    bool used;
+    while (true) {
+        QString message;
+        if (used) {
+            message.append("That name was already used, please, choose another:");
+        } else {
+            message.append("Enter the name of the tab:");
+        }
+        bool ok;
+        name = QInputDialog::getText(this, "Enter the name", message, QLineEdit::Normal, actualName, &ok);
+        if (!ok) {
+            return;
+        }
+        if (name.isEmpty()) {
+            name = actualName;
+        }
+        used = tabAlreadyExists(name);
+        if (!used && name != actualName) {
+            break;
+        }
+    }
+    tab->setObjectName("tab_"+name);
+    tabWidget->setTabText(currentIndex, name);
+}
+
+void MainWindow::createNewAutomata(FiniteAutomata f) {
+    bool ok;
+    QString freeName = findFreeName("FA");
+    QString name;
+    bool used;
+    while (true) {
+        QString message;
+        if (used) {
+            message.append("That name was already used, please, choose another:");
+        } else {
+            message.append("Enter the name of the new Finite Automata");
+        }
+        name = QInputDialog::getText(this, "Enter the name", message, QLineEdit::Normal, freeName, &ok);
+        if (!ok) {
+            return;
+        }
+        if (name.isEmpty()) {
+            name = freeName;
+        }
+        used = tabAlreadyExists(name);
+        if (!used) {
+            break;
+        }
+    }
+    FiniteAutomataTab *tab = new FiniteAutomataTab();
+    tab->fromAutomata(f);
+    tab->setObjectName("tab_"+name);
+    this->tabWidget->setCurrentIndex(this->tabWidget->addTab(tab, name));
+}
+
+void MainWindow::createNewRegularExpression() {
+    bool ok;
+    QString freeName = findFreeName("RE");
+    QString name;
+    bool used;
+    while (true) {
+        QString message;
+        if (used) {
+            message.append("That name was already used, please, choose another:");
+        } else {
+            message.append("Enter the name of the new Regular Expression");
+        }
+        name = QInputDialog::getText(this, "Enter the name", message, QLineEdit::Normal, freeName, &ok);
+        if (!ok) {
+            return;
+        }
+        if (name.isEmpty()) {
+            name = freeName;
+        }
+        used = tabAlreadyExists(name);
+        if (!used) {
+            break;
+        }
+    }
+    FiniteAutomataTab *tab = new FiniteAutomataTab();
+    tab->setObjectName("tab_"+name);
+    this->tabWidget->addTab(tab, name);
+}
+
+QString MainWindow::findFreeName(QString prefix) {
+    int i = 1;
+    bool isFree = false;
+    QString name;
+    do {
+        name = prefix + QString::number(i);
+        isFree = !tabAlreadyExists(name);
+        i++;
+    } while (!isFree);
+    return name;
+}
+
+bool MainWindow::tabAlreadyExists(QString name) {
+    return tabWidget->findChild<FiniteAutomataTab*>("tab_"+name) != 0;
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -1,10 +1,13 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "all.h"
-
 #include <QMainWindow>
 #include <QApplication>
+#include <QInputDialog>
+#include <QMenu>
+#include <QAction>
+#include "finite_automata.h"
+
 
 namespace Ui {
 class MainWindow;
@@ -15,11 +18,101 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    /*!
+     * Constructs the MainWindow, setting up the object
+     * and a few other things
+     *
+     * @param parent The parent widget, if any
+     */
     explicit MainWindow(QWidget *parent = 0);
+
+    /*!
+     * Destructs the object and free memory, if any
+     */
     ~MainWindow();
 
+public slots:
+    /*!
+     * Method that is called when the user requests
+     * the creation of a new finite automata.
+     * Please note that this method asks the user for
+     * a name, suggesting a name not used yet, and
+     * simply returns if the user cancels the creation
+     * of the finite automata.
+     */
+    void createNewAutomata();
+
+    /*!
+     * Method that is called when the user requests
+     * the loading of a existing Finite Automata
+     * Please note that this method asks the user for
+     * a name, suggesting a name not used yet, and
+     * simply returns if the user cancels the creation
+     * of the tab for that finite automata.
+     */
+    void createNewAutomata(FiniteAutomata f);
+
+    /*!
+     * Method that is called when the user requests
+     * the creation of a new regular expression.
+     * Please note that this method asks the user for
+     * a name, suggesting a name not used yet, and
+     * simply returns if the user cancels the creation
+     * of the regular expression.
+     */
+    void createNewRegularExpression();
+
+    /*!
+     * Set the name of a tab, asking for the user to enter
+     * it
+     */
+    void setTabName();
+
+    /*!
+     * Do the union of two automatas, asking for the other
+     * automata first
+     */
+    void doUnion();
+
+    /*!
+     * Do the union of two automatas, asking for the other
+     * automata first
+     */
+    void doIntersection();
+
+    /*!
+     * Fix the menus, enabling and disabling options as needed
+     *
+     * @param currentIndex The actual tab enabled
+     */
+    void fixMenus(int currentIndex);
 private:
-    Ui::MainWindow *ui;
+    /*!
+     * Find a free name for a tab
+     *
+     * @param prefix The prefix of the tab
+     * @return A name that was not used
+     */
+    QString findFreeName(QString prefix);
+
+    /*!
+     * Check if a tab already exists
+     *
+     * @param tabName The name to check for existence
+     * @return true if the tab already exists, false otherwise
+     */
+    bool tabAlreadyExists(QString tabName);
+
+    /*!
+     * Return a list of the automatas actually opened
+     * in the window
+     *
+     * @return A list of the names of automatas actually opened
+     */
+    QStringList getAutomatasNames();
+
+    Ui::MainWindow *ui; //!< A pointer to the Qt Main Window
+    QTabWidget *tabWidget; //!< A pointer to the tab widget present in the main window
 };
 
 #endif // MAINWINDOW_H
