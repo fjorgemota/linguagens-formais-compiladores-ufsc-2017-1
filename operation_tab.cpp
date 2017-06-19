c4bcd59efceea1f46a8c62ea1cbd9def33a40e60
Fernando Jorge Mota <f.j.mota13@gmail.com>
2017-06-18 23:04:52 -0300

    Remove the last column and row from the table in the OperationTab (so we have a really thin table)

--- operation_tab.cpp
+++ operation_tab.cpp
@@ -13,13 +13,15 @@ void OperationTab::addStep(MainWindow *window, FiniteAutomata f, QString title) 
     table->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     table->fromAutomata(f);
     int rowHeight = 0;
-    int tableRowCount = table->rowCount();
+    int tableRowCount = table->rowCount()-1;
     for (int c=0; c<tableRowCount; c++) {
         rowHeight += table->rowHeight(c);
     }
     table->setMinimumHeight(rowHeight);
     table->setMaximumHeight(rowHeight);
     table->setReadOnly();
+    table->removeColumn(table->columnCount()-1);
+    table->removeRow(tableRowCount);
     gridLayout->addWidget(table, rowCount+1, 0);
     MoveToTabButton *button = new MoveToTabButton(this, window, f);
     gridLayout->addWidget(button, rowCount, 1, 2, 1);
